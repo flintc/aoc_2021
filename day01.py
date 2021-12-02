ce8847f4a6821164f5d9df25988a5256fae5cd50
C Flint <flintc27@gmail.com>
2021-12-01 21:31:52 -0500

    updated day 1 solutions to use pandas b/c why not

--- day01.py
+++ day01.py
@@ -1,12 +1,7 @@
 from common import *
 
-raw_data = get_input(1, 2021)
-
-measurements = [int(x) for x in raw_data.split("\n") if len(x) > 0]
-increased, = np.where(np.diff(measurements)>0)
-print("Day 1, part 1: ", len(increased))
-
-measurements = [int(x.split(" ")[0]) for x in raw_data.split("\n") if len(x) > 0]
-measurements_rolling_avg = np.convolve(measurements, np.ones(3), 'valid')
-increased, = np.where(np.diff(measurements_rolling_avg)>0)
-print("Day 1, part 2: ", len(increased))
+measurements = pd.DataFrame(
+    dict(x=[int(x) for x in get_input(1, 2021).split("\n") if len(x) > 0])
+)
+print("Day 1, part 1: ", measurements.diff().query("x > 0").size)
+print("Day 1, part 2: ", measurements.rolling(3).mean().diff().query("x > 0").size)
