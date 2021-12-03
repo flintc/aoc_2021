f97f408a0ffafa002a6940dd598a5541926c0ff5
C Flint <flintc27@gmail.com>
2021-12-03 07:38:04 -0500

    minor cleanup

--- day03.py
+++ day03.py
@@ -1,6 +1,10 @@
 from common import *
 
 
+def to_dec(df):
+    return int("".join(df.values.flatten()), base=2)
+
+
 def get_rating(df, least_common=False, tie_breaker='1'):
     new_df = df.copy()
     inc = 0
@@ -15,21 +19,17 @@ def get_rating(df, least_common=False, tie_breaker='1'):
         if new_df.shape[0] == 1:
             break
         inc += 1
-    return int("".join(new_df.values.flatten()), base=2)
+    return new_df.pipe(to_dec)
 
 
 def main():
     raw_data = get_input(3)
     df = pd.DataFrame([list(x) for x in raw_data.split("\n") if len(x) > 0])
-
     value_counts = df.apply(pd.Series.value_counts)
-    gamma = int("".join(value_counts.apply(
-        lambda col: col.sort_values().index[-1]).values), base=2)
-    epsilon = int("".join(value_counts.apply(
-        lambda col: col.sort_values().index[0])), base=2)
+    gamma = value_counts.apply(lambda col: col.sort_values().index[-1]).pipe(to_dec)
+    epsilon = value_counts.apply(lambda col: col.sort_values().index[0]).pipe(to_dec)
 
     print("Day 3, part 1: ", gamma * epsilon)
-
     print("Day 3, part 2: ", get_rating(df, False, '1')*get_rating(df, True, '0'))
 
 
