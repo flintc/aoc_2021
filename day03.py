1556617f72f089a2ac65c5bc9ba49869d89fb599
C Flint <flintc27@gmail.com>
2021-12-03 10:06:02 -0500

    functionize part 1

--- day03.py
+++ day03.py
@@ -1,36 +1,34 @@
 from common import *
 
 
-def to_dec(df):
+def binary_df_to_decimal(df):
     return int("".join(df.values.flatten()), base=2)
+
+
+def get_rate(df, least_common=False):
+    return (df.apply(pd.Series.value_counts)
+              .apply(lambda col: col.sort_values(ascending=least_common).index[0])
+              .pipe(binary_df_to_decimal))
 
 
 def get_rating(df, least_common=False, tie_breaker='1'):
     new_df = df.copy()
     inc = 0
-    max_pos = len(df.columns)
     while True:
-        pos = inc % max_pos
+        pos = inc % len(df.columns)
         value_counts = new_df[pos].value_counts(ascending=least_common)
-        if value_counts.unique().size == 1:
-            new_df = new_df.loc[new_df[pos] == tie_breaker]
-        else:
-            new_df = new_df.loc[new_df[pos] == value_counts.index[0]]
+        keep = tie_breaker if value_counts.unique().size == 1 else value_counts.index[0]
+        new_df = new_df.loc[new_df[pos] == keep]
         if new_df.shape[0] == 1:
             break
         inc += 1
-    return new_df.pipe(to_dec)
+    return new_df.pipe(binary_df_to_decimal)
 
 
 def main():
-    raw_data = get_input(3)
-    df = pd.DataFrame([list(x) for x in raw_data.split("\n") if len(x) > 0])
-    value_counts = df.apply(pd.Series.value_counts)
-    gamma = value_counts.apply(lambda col: col.sort_values().index[-1]).pipe(to_dec)
-    epsilon = value_counts.apply(lambda col: col.sort_values().index[0]).pipe(to_dec)
-
-    print("Day 3, part 1: ", gamma * epsilon)
-    print("Day 3, part 2: ", get_rating(df, False, '1')*get_rating(df, True, '0'))
+    df = pd.DataFrame([list(x) for x in get_input(3).split("\n") if len(x) > 0])
+    print("Day 3, part 1: ", get_rate(df, False) * get_rate(df, True))
+    print("Day 3, part 2: ", get_rating(df, False, '1') * get_rating(df, True, '0'))
 
 
 if __name__ == "__main__":
